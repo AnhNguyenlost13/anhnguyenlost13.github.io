555d3f1856a4a6a593806922e367364d013ee7f3
AnhNguyenlost13 <94160753+AnhNguyenlost13@users.noreply.github.com>
2024-03-05 19:24:12 +0700

    Update script.js

--- testing/script.js
+++ testing/script.js
@@ -1,3 +1,6 @@
+// hello!
+
+// This is to make sure that everything has loaded. May be paranoia.
 document.addEventListener('DOMContentLoaded', function() {
 
 // Define the strings
@@ -11,22 +14,29 @@ const internalIdentifiers = ['content_en', 'content_ro'];
 // Initial language index
 let currentLanguageIndex = 0;
 
+// convenience/shortening
 const langSelector = document.querySelector('.lang_selector');
 
-// Listen for click event on lang_selector
+// Listen for a click event
 langSelector.addEventListener('click', function() {
     // Toggle between the languages on click
     currentLanguageIndex = (currentLanguageIndex + 1) % internalIdentifiers.length;
+    
+    // Getting the string from the index
     const selectedLanguage = prettyLanguages[currentLanguageIndex];
     const selectedLanguageIdentifier = internalIdentifiers[currentLanguageIndex];
     const pageTitle = pageTitles[currentLanguageIndex];
     const readTime = readTimes[currentLanguageIndex];
+    
+    // Changing the strings
     document.querySelector('.selected_language').textContent = selectedLanguage;
     document.querySelector('.title').textContent = pageTitle;
     document.querySelector('.readtime').textContent = readTime;
+    
+    // Yes I did not forget the page title.
     document.title = pageTitle;
     
-    // Call a function to update the page content based on the selected language
+    // Call a function to update the big chungus page content based on the selected language
     updateContent(selectedLanguageIdentifier);
 });
 
